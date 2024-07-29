aefc3a5f7f1e79aaf36abe84c3b674d9da94daca
engAhmedSami <xotornado@gmail.com>
2024-07-29 16:56:19 +0300

    "Added Firebase Core configuration to Android, macOS, and Windows platforms, and updated pubspec.yaml and pubspec.lock files to include firebase_core dependency."

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:newsapp/Core/helper_function/on_generate_routes.dart';
 import 'package:newsapp/Core/services/shared_preferences_sengleton.dart';
 import 'package:newsapp/Core/utils/app_colors.dart';
+import 'package:newsapp/firebase_options.dart';
 import 'package:newsapp/generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Prfs.init();
   runApp(const NewsApp());
 }
