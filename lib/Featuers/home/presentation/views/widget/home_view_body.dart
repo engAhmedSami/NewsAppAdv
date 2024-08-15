d363415550559d6deee261aef02dff5732fe6989
engAhmedSami <xotornado@gmail.com>
2024-08-15 19:08:57 +0300

    replace

--- lib/Featuers/home/presentation/views/widget/home_view_body.dart
+++ lib/Featuers/home/presentation/views/widget/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:newsapp/Featuers/home/presentation/user_info/persentation/views/widget/user_info_list_tile.dart';
+import 'package:newsapp/Featuers/home/user_info/persentation/views/widget/user_info_list_tile.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
