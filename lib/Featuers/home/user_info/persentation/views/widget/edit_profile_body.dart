3a23a88d4b34b5e72ba20a71556b8f0a8f24337f
engAhmedSami <xotornado@gmail.com>
2024-08-17 03:19:58 +0300

    "Updated dependencies, added new packages, and modified code in various files."

--- lib/Featuers/home/user_info/persentation/views/widget/edit_profile_body.dart
+++ lib/Featuers/home/user_info/persentation/views/widget/edit_profile_body.dart
@@ -1,5 +1,139 @@
+// // ignore_for_file: use_build_context_synchronously
+
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
+// import 'package:newsapp/Core/widget/custom_botton.dart';
+// import 'package:newsapp/Core/widget/custom_text_field.dart';
+// import 'package:newsapp/Featuers/auth/presentation/view/verfied_email_view.dart';
+// import 'package:newsapp/Featuers/home/user_info/data/user_info_model.dart';
+
+// class EditUserInfoView extends StatefulWidget {
+//   final String uid;
+//   final UserInfoModel userInfo;
+
+//   const EditUserInfoView({
+//     super.key,
+//     required this.uid,
+//     required this.userInfo,
+//     required Null Function() onUserInfoUpdated,
+//   });
+
+//   @override
+//   EditUserInfoViewState createState() => EditUserInfoViewState();
+// }
+
+// class EditUserInfoViewState extends State<EditUserInfoView> {
+//   late TextEditingController nameController;
+//   late TextEditingController bioController;
+//   File? profileImage;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     nameController = TextEditingController(text: widget.userInfo.name);
+//     bioController = TextEditingController(text: widget.userInfo.bio);
+//   }
+
+//   Future<void> _pickImage() async {
+//     final pickedFile =
+//         await ImagePicker().pickImage(source: ImageSource.gallery);
+//     if (pickedFile != null) {
+//       setState(() {
+//         profileImage = File(pickedFile.path);
+//       });
+//     }
+//   }
+
+//   Future<String?> _uploadImageToFirebase(String filePath) async {
+//     try {
+//       final storageRef = FirebaseStorage.instance.ref();
+//       final file = File(filePath);
+//       final fileName = file.uri.pathSegments.last;
+//       final imageRef = storageRef.child("profile_pics/$fileName");
+//       final uploadTask = imageRef.putFile(file);
+//       final snapshot = await uploadTask.whenComplete(() => null);
+//       final downloadUrl = await snapshot.ref.getDownloadURL();
+//       return downloadUrl;
+//     } catch (e) {
+//       debugPrint("Error uploading image: $e");
+//       return null;
+//     }
+//   }
+
+//   Future<void> _updateUserInfo() async {
+//     String? profilePicUrl = widget.userInfo.profilePic;
+
+//     if (profileImage != null) {
+//       profilePicUrl = await _uploadImageToFirebase(profileImage!.path);
+//     }
+
+//     final updatedUserInfo = {
+//       'name': nameController.text,
+//       'bio': bioController.text,
+//       'profilePic': profilePicUrl,
+//     };
+
+//     await FirebaseFirestore.instance
+//         .collection('users')
+//         .doc(widget.uid)
+//         .update(updatedUserInfo);
+
+//     if (context.mounted) {
+//       Navigator.of(context).pop(true); // Notify parent widget of the update
+//     }
+//   }
+
 // ignore_for_file: use_build_context_synchronously
 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: buildAppBarC(
+//         title: 'Edit Profile',
+//         context,
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           children: [
+//             const SizedBox(height: 24),
+//             GestureDetector(
+//               onTap: _pickImage,
+//               child: CircleAvatar(
+//                 radius: 50,
+//                 backgroundImage: profileImage != null
+//                     ? FileImage(profileImage!)
+//                     : NetworkImage(widget.userInfo.profilePic ?? ''),
+//               ),
+//             ),
+//             const SizedBox(height: 24),
+//             CustomTextFormField(
+//               hintText: 'Name',
+//               controller: nameController,
+//               prefixIcon: const Icon(Icons.person, color: Colors.grey),
+//               textInputType: TextInputType.name,
+//             ),
+//             const SizedBox(height: 24.0),
+//             CustomTextFormField(
+//               hintText: 'Bio',
+//               controller: bioController,
+//               prefixIcon: const Icon(Icons.info, color: Colors.grey),
+//               textInputType: TextInputType.name,
+//             ),
+//             const SizedBox(height: 24),
+//             CustomBotton(
+//               text: 'Update',
+//               onPressed: _updateUserInfo,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -13,12 +147,13 @@ import 'package:newsapp/Featuers/home/user_info/data/user_info_model.dart';
 class EditUserInfoView extends StatefulWidget {
   final String uid;
   final UserInfoModel userInfo;
+  final VoidCallback onUserInfoUpdated;
 
   const EditUserInfoView({
     super.key,
     required this.uid,
     required this.userInfo,
-    required Null Function() onUserInfoUpdated,
+    required this.onUserInfoUpdated,
   });
 
   @override
@@ -66,23 +201,34 @@ class EditUserInfoViewState extends State<EditUserInfoView> {
   Future<void> _updateUserInfo() async {
     String? profilePicUrl = widget.userInfo.profilePic;
 
-    if (profileImage != null) {
-      profilePicUrl = await _uploadImageToFirebase(profileImage!.path);
-    }
+    try {
+      if (profileImage != null) {
+        profilePicUrl = await _uploadImageToFirebase(profileImage!.path);
+      }
 
-    final updatedUserInfo = {
-      'name': nameController.text,
-      'bio': bioController.text,
-      'profilePic': profilePicUrl,
-    };
+      final updatedUserInfo = {
+        'name': nameController.text,
+        'bio': bioController.text,
+        'profilePic': profilePicUrl,
+      };
 
-    await FirebaseFirestore.instance
-        .collection('users')
-        .doc(widget.uid)
-        .update(updatedUserInfo);
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(widget.uid)
+          .update(updatedUserInfo);
 
-    if (context.mounted) {
-      Navigator.of(context).pop(true); // Notify parent widget of the update
+      widget.onUserInfoUpdated();
+
+      if (context.mounted) {
+        Navigator.of(context).pop(true); // Notify parent widget of the update
+      }
+    } catch (e) {
+      debugPrint("Error updating user info: $e");
+
+      // Handle specific errors like 403 and refresh token if necessary
+      if (e is FirebaseException && e.code == 'storage/unauthorized') {
+        // handle token regeneration or notify the user
+      }
     }
   }
 
--- lib/Featuers/home/user_info/persentation/views/widget/user_info_view_body.dart
+++ lib/Featuers/home/user_info/persentation/views/widget/user_info_view_body.dart
@@ -1,8 +1,138 @@
+// // ignore_for_file: use_build_context_synchronously
+
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:newsapp/Core/utils/app_colors.dart';
+// import 'package:newsapp/Core/widget/custom_botton.dart';
+// import 'package:newsapp/Core/widget/custom_text_field.dart';
+// import 'package:newsapp/Featuers/home/user_info/persentation/save_info_cubit/profile_cubit.dart';
+// import 'package:newsapp/Featuers/home/user_info/persentation/views/show_snck_bar.dart';
+// import 'package:newsapp/Featuers/home/presentation/views/nav_bar.dart';
+// import 'package:newsapp/constants.dart';
+
+// class UserInfoViewBody extends StatefulWidget {
+//   const UserInfoViewBody({super.key});
+
+//   @override
+//   State<UserInfoViewBody> createState() => _UserInfoViewBodyState();
+// }
+
+// class _UserInfoViewBodyState extends State<UserInfoViewBody> {
+//   File? image;
+//   final TextEditingController nameController = TextEditingController();
+//   final TextEditingController emailController = TextEditingController();
+//   final TextEditingController bioController = TextEditingController();
+
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     nameController.dispose();
+//     emailController.dispose();
+//     bioController.dispose();
+//   }
+
+//   void selectImage() async {
+//     image = await pichImage(context);
+//     setState(() {});
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Padding(
+//         padding: const EdgeInsets.symmetric(horizontal: kHorizintalPadding),
+//         child: SingleChildScrollView(
+//           child: Center(
+//             child: Column(
+//               children: [
+//                 InkWell(
+//                   onTap: () {
+//                     selectImage();
+//                   },
+//                   child: image == null
+//                       ? const CircleAvatar(
+//                           backgroundColor: AppColors.secondaryColor,
+//                           radius: 70,
+//                           child: Icon(
+//                             Icons.account_circle,
+//                             color: AppColors.primaryColor,
+//                             size: 70,
+//                           ),
+//                         )
+//                       : CircleAvatar(
+//                           radius: 70,
+//                           backgroundImage: FileImage(image!),
+//                         ),
+//                 ),
+//                 const SizedBox(
+//                   height: 20,
+//                 ),
+//                 CustomTextFormField(
+//                   prefixIcon:
+//                       const Icon(Icons.person, color: AppColors.secondaryColor),
+//                   textInputType: TextInputType.name,
+//                   controller: nameController,
+//                   hintText: 'Enter Your Name',
+//                 ),
+//                 const SizedBox(
+//                   height: 20,
+//                 ),
+//                 CustomTextFormField(
+//                   prefixIcon:
+//                       const Icon(Icons.email, color: AppColors.secondaryColor),
+//                   textInputType: TextInputType.emailAddress,
+//                   controller: emailController,
+//                   hintText: 'Enter Your Email',
+//                 ),
+//                 const SizedBox(
+//                   height: 20,
+//                 ),
+//                 CustomTextFormField(
+//                   prefixIcon:
+//                       const Icon(Icons.info, color: AppColors.secondaryColor),
+//                   textInputType: TextInputType.name,
+//                   controller: bioController,
+//                   hintText: 'Enter Your Bio',
+//                 ),
+//                 const SizedBox(
+//                   height: 20,
+//                 ),
+//                 CustomBotton(
+//                   onPressed: () {
+//                     stroeData();
+
+//                     Navigator.pushNamed(
+//                       context,
+//                       HomeView.routeName,
+//                     );
+//                   },
+//                   text: 'Save',
+//                 )
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
 // ignore_for_file: use_build_context_synchronously
 
+//   void stroeData() async {
+//     await context.read<ProfileCubit>().saveUserInfo(
+//         name: nameController.text,
+//         email: emailController.text,
+//         bio: bioController.text,
+//         profilePic: image!.path);
+//     if (image != null) {
+//     } else {}
+//   }
+// }
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:newsapp/Core/helper_function/failuer_top_snak_bar.dart';
 import 'package:newsapp/Core/utils/app_colors.dart';
 import 'package:newsapp/Core/widget/custom_botton.dart';
 import 'package:newsapp/Core/widget/custom_text_field.dart';
@@ -26,10 +156,10 @@ class _UserInfoViewBodyState extends State<UserInfoViewBody> {
 
   @override
   void dispose() {
-    super.dispose();
     nameController.dispose();
     emailController.dispose();
     bioController.dispose();
+    super.dispose();
   }
 
   void selectImage() async {
@@ -65,9 +195,7 @@ class _UserInfoViewBodyState extends State<UserInfoViewBody> {
                           backgroundImage: FileImage(image!),
                         ),
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
+                const SizedBox(height: 20),
                 CustomTextFormField(
                   prefixIcon:
                       const Icon(Icons.person, color: AppColors.secondaryColor),
@@ -75,9 +203,7 @@ class _UserInfoViewBodyState extends State<UserInfoViewBody> {
                   controller: nameController,
                   hintText: 'Enter Your Name',
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
+                const SizedBox(height: 20),
                 CustomTextFormField(
                   prefixIcon:
                       const Icon(Icons.email, color: AppColors.secondaryColor),
@@ -85,9 +211,7 @@ class _UserInfoViewBodyState extends State<UserInfoViewBody> {
                   controller: emailController,
                   hintText: 'Enter Your Email',
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
+                const SizedBox(height: 20),
                 CustomTextFormField(
                   prefixIcon:
                       const Icon(Icons.info, color: AppColors.secondaryColor),
@@ -95,20 +219,20 @@ class _UserInfoViewBodyState extends State<UserInfoViewBody> {
                   controller: bioController,
                   hintText: 'Enter Your Bio',
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
+                const SizedBox(height: 20),
                 CustomBotton(
-                  onPressed: () {
-                    stroeData();
+                  onPressed: () async {
+                    await stroeData();
 
-                    Navigator.pushNamed(
-                      context,
-                      HomeView.routeName,
-                    );
+                    if (mounted) {
+                      Navigator.pushNamed(
+                        context,
+                        HomeView.routeName,
+                      );
+                    }
                   },
                   text: 'Save',
-                )
+                ),
               ],
             ),
           ),
@@ -117,13 +241,16 @@ class _UserInfoViewBodyState extends State<UserInfoViewBody> {
     );
   }
 
-  void stroeData() async {
-    await context.read<ProfileCubit>().saveUserInfo(
-        name: nameController.text,
-        email: emailController.text,
-        bio: bioController.text,
-        profilePic: image!.path);
+  Future<void> stroeData() async {
     if (image != null) {
-    } else {}
+      await context.read<ProfileCubit>().saveUserInfo(
+          name: nameController.text,
+          email: emailController.text,
+          bio: bioController.text,
+          profilePic: image!.path);
+    } else {
+      // Handle case where image is null, maybe show an error or use a default image
+      failuerTopSnackBar(context, "Please select an image");
+    }
   }
 }
